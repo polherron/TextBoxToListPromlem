77c9356b3a6a01dd160de42fa79b953ee2718282
Paul Herron <paul.heron@nln.ie>
2019-05-13 13:44:37 +0100

    Added Text Box

--- TextBoxToListPromlem/Form1.Designer.cs
+++ TextBoxToListPromlem/Form1.Designer.cs
@@ -47,6 +47,7 @@
             this.textBox17 = new System.Windows.Forms.TextBox();
             this.textBox18 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox19 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -185,11 +186,19 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox19
+            // 
+            this.textBox19.Location = new System.Drawing.Point(198, 202);
+            this.textBox19.Name = "textBox19";
+            this.textBox19.Size = new System.Drawing.Size(100, 20);
+            this.textBox19.TabIndex = 19;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(483, 450);
+            this.Controls.Add(this.textBox19);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox10);
             this.Controls.Add(this.textBox11);
@@ -237,6 +246,7 @@
         private System.Windows.Forms.TextBox textBox17;
         private System.Windows.Forms.TextBox textBox18;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox19;
     }
 }
 
